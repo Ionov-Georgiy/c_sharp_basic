850d571f0ba84acf790ddf6658e854b62f15baa7
Ionov-Georgiy <74145892+Ionov-Georgiy@users.noreply.github.com>
2020-12-23 17:53:18 +0600

    Add files via upload

---
+++ FourthLessonTasks/ArrayClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthLessonTasks
+{
+    class ArrayClass
+    {
+        int[] a;
+        //  Создание массива и заполнение его одним значением el  
+        public ArrayClass(int n, int el)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = el;
+        }
+        //  Создание массива и заполнение его случайными числами от min до max
+        public ArrayClass(int n, int min, int max)
+        {
+            a = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(min, max);
+        }
+        // Создание массива и заполнение его числами в арифметической последовательности от from до to с шагом step
+        public ArrayClass(double from, double to, int step)
+        {
+            int n = (int)Math.Floor((from - to) / step) + 1;
+            int arrLength = n + 1;
+            a = new int[arrLength];
+            int arrElem = (int)from;
+            int i = 0;
+            while(arrElem <= to)
+            {
+                a[i] = arrElem;
+                arrElem += step;
+                i++;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] > max) max = a[i];
+                return max;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = a[0];
+                for (int i = 1; i < a.Length; i++)
+                    if (a[i] < min) min = a[i];
+                return min;
+            }
+        }
+        public int CountPositiv
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length; i++)
+                    if (a[i] > 0) count++;
+                return count;
+            }
+        }
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in a)
+                s = s + v + "; ";
+            return s;
+        }
+    }
+}
---
+++ FourthLessonTasks/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourthLessonTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1) Дан  целочисленный  массив  из 20 элементов.  Элементы  массива  могут принимать  целые  значения  от –10 000 до 10 000 включительно. Заполнить случайными числами.  Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих элемента массива. Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.
+            FirstTask();
+
+            // 2)
+            SecondTask();
+
+
+        }
+
+        static void FirstTask()
+        {
+
+            short[] arr = new short[20];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (short)rand.Next(-10000, 10000);
+            }
+            int counter = 0;
+            string str = "";
+            for(int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i] % 3 == 0 && arr[i+1] % 3 != 0)
+                {
+                    str += arr[i] + " и " + arr[i+1] + "; ";
+                    counter++;
+                }
+                else if(arr[i] % 3 != 0 && arr[i + 1] % 3 == 0)
+                {
+                    str += arr[i] + " и " + arr[i + 1] + "; ";
+                    counter++;
+                }
+            }
+            Console.WriteLine(str);
+            Console.WriteLine("Итого: " + counter);
+            Console.ReadKey();
+        }
+
+        static void SecondTask()
+        {
+            Random rand = new Random();
+            double from = 1d;
+            double to = rand.Next(1, 100);
+            int step = rand.Next(1, 10);
+
+            Console.WriteLine("От: "+from+"До: "+to+"Шаг: "+step);
+
+            ArrayClass nwArCls = new ArrayClass(from, to, step);
+            Console.WriteLine("Итог: " + nwArCls.ToString());
+        }
+
+    }
+}
